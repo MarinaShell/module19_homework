f6381b7076015c560d4124016b84fc52f5337413
Ponka Petrova <marinashell@mail.ru>
2022-10-26 22:46:15 +0300

    finish all tasks

--- js/task5.js
+++ js/task5.js
@@ -1,69 +1,58 @@
 /*****************************************************************************/
-/*родительская функция с методами, 
+/*родительский класс с методами, 
 которые включают/выключают прибор из розетки*/
-function Electropribor(name, price, power){
-    this.name = name;
-    this.price = price;
-    this.power = power;
+class Electropribor{
+    constructor(name, price, power){
+        this.name = name;
+        this.price = price;
+        this.power = power;
+    }
+
+    /*метод, который выключает прибор из розетки*/
+    turnOff(){
+        console.log(`${this.name} is turnOff`);
+    }
+    /*метод, который включает прибор из розетки*/
+    turnOn(){
+        console.log(`${this.name} is turnOn`);    
+    }
+    /*метод, который сообщает об объекте его цену*/
+    getPrice(){
+        console.log(`The price of ${this.name} is ${this.price}`);    
+    }
+    /*метод, который сообщает об объекте его мощность*/
+    getPower(){
+        console.log(`The power of ${this.name} is ${this.power}`);    
+    }
+    
 }
+
+/*класс электрический чайник*/
+class ElectricKettle extends Electropribor{
+    constructor(size, name, price, power){
+        super(name, price, power);  
+        this.size = size;    
+    }
+    /*метод возврата размера для электрического чайника*/
+    getSize(){
+        console.log(`The size of ${this.name} is ${this.size}`);
+    }
+}
+
+/*класс микроволновка*/
+class Microwave extends Electropribor{
+    constructor(type, name, price, power){
+        super(name, price, power)
+        this.type = type;
+    }
+    /*метод возврата типа для микроволновки*/
+    getType(){
+        console.log(`The type of ${this.name} is ${this.type}`);
+    }
+}
+
 
 /*****************************************************************************/
-/*метод, который выключает прибор из розетки*/
-Electropribor.prototype.turnOff = function(){
-    console.log(`${this.name} is turnOff`);
-}
-
-/*****************************************************************************/
-/*метод, который включает прибор из розетки*/
-Electropribor.prototype.turnOn = function(){
-    console.log(`${this.name} is turnOn`);    
-}
-
-/*****************************************************************************/
-/*метод, который сообщает об объекте его цену*/
-Electropribor.prototype.getPrice = function(){
-    console.log(`The price of ${this.name} is ${this.price}`);    
-}
-
-/*****************************************************************************/
-/*метод, который сообщает об объекте его мощность*/
-Electropribor.prototype.getPower = function(){
-    console.log(`The power of ${this.name} is ${this.power}`);    
-}
-
-/*****************************************************************************/
-/*электрический чайник*/
-function ElectricKettle(size, name, price, power){
-    Electropribor.call(this, name, price, power);
-    this.size = size;    
-}
-
-/*делаем прототип ElectricKettle от Electropribor*/
-ElectricKettle.prototype = Object.create(Electropribor.prototype);
-ElectricKettle.prototype.constructor = ElectricKettle;
-
-/*****************************************************************************/
-/*метод возврата размера для электрического чайника*/
-ElectricKettle.prototype.getSize = function(){
-    console.log(`The size of ${this.name} is ${this.size}`);
-}
-
-/*****************************************************************************/
-/*микроволновка*/
-function Microwave(type, name, price, power){
-    Electropribor.call(this, name, price, power);
-    this.type = type;
-}
-
-/*делаем прототип Microwave от Electropribor*/
-Microwave.prototype = Object.create(Electropribor.prototype);
-Microwave.prototype.constructor = Microwave;
-
-/*****************************************************************************/
-/*метод возврата типа для микроволновки*/
-Microwave.prototype.getType = function(){
-    console.log(`The type of ${this.name} is ${this.type}`);
-}
 
 /*for testing*/
 
